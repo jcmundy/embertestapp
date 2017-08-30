23e4df33441da0a14836283de94011f66776c69a
Joanna <joanna@jcmundy.library.emory.ddns>
2017-08-30 16:10:34 -0400

    author hash

--- frontend/app/models/author.js
+++ frontend/app/models/author.js
@@ -1,9 +1,10 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   books: DS.hasMany('book'),
 
-  isNotValid: Ember.computed.empty('name'),
+  isNotValid: Ember.computed.empty('name')
 
 });
--- frontend/app/routes/authors.js
+++ frontend/app/routes/authors.js
@@ -2,11 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('author');
+   return Ember.RSVP.hash({
+   author: this.store.findAll('author'),
+   book: this.store.findAll('book')
+  });
   },
-  model() {
-    return this.store.findAll('book');
+
+  setupController(controller, models) {
+    controller.set('author', models.author);
+    controller.set('book', models.book);
   },
+
   actions: {
 
     editAuthor(author) {
